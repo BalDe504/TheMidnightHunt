55af6d3913b0376baec1fa06979e636a50a02cc6
Damian Nowak <damian.nowak@microsoft.wsei.edu.pl>
2023-08-29 13:52:46 +0200

    Audio Scripts
    Added Audio and Subtitles trigger scripts

---
+++ Assets/Scripts/Vocals.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vocals : MonoBehaviour
+{
+    private AudioSource source;
+    public static Vocals instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void Say(AudioObject clip)
+    {
+        if (source.isPlaying)
+            source.Stop();
+
+        source.PlayOneShot(clip.clip);
+
+        UI.instance.SetSubtitle(clip.subtitle, clip.clip.length);
+    }
+}
